b33b7b211799325bc1b0ee39ccb969fdced2fda0
AbraaoMorais <76604710+AbraaoMorais@users.noreply.github.com>
2021-01-31 23:28:29 -0300

    Delete Sites directory

--- Sites/README.md
+++
@@ -1,2 +0,0 @@
-# Academic_HTML5_CSS3_JS
-Projetos acadêmicos com as linguagens de programação HTML5, CSS3 e JavaScript.
--- Sites/website_1/js/bloqclick.js
+++
@@ -1,7 +0,0 @@
-window.onload = function(){
-	document.getElementById("bloqclick").oncontextmenu = function()
-	{
-	//alert("earaaaaa");
-	return false;
-		}
-	}
